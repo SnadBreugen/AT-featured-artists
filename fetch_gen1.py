0fcb2c65bae4ef197c969cb95e3f2f065d12a9e5
SnadBreugen <ralf.noetzel@audiotool.com>
2026-04-14 16:57:17 +0200

    Gen I artist page: portrait + cover with bar

--- fetch_gen1.py
+++ fetch_gen1.py
@@ -5,111 +5,111 @@ import re
 import datetime
 
 albums = [
-    "https://www.audiotool.com/album/9pnx5/",
-    "https://www.audiotool.com/album/hnwkpu4/",
-    "https://www.audiotool.com/album/dgaoqtpm/",
-    "https://www.audiotool.com/album/0poc9tl/",
-    "https://www.audiotool.com/album/6pvw3rdg/",
-    "https://www.audiotool.com/album/i0qv2qyt6/",
-    "https://www.audiotool.com/album/zwhdmh/",
-    "https://www.audiotool.com/album/agr87qx/",
-    "https://www.audiotool.com/album/pts4e/",
-    "https://www.audiotool.com/album/2gvqxec/",
-    "https://www.audiotool.com/album/dfkvkal/",
-    "https://www.audiotool.com/album/trxsjo/",
-    "https://www.audiotool.com/album/rpzwcp4v/",
-    "https://www.audiotool.com/album/xlv4lr4/",
-    "https://www.audiotool.com/album/amg2pbi/",
-    "https://www.audiotool.com/album/og6fd6z/",
-    "https://www.audiotool.com/album/kcvqjvpm2/",
-    "https://www.audiotool.com/album/8ktqn1orf/",
-    "https://www.audiotool.com/album/t5tdmds/",
-    "https://www.audiotool.com/album/76ipsvb/",
-    "https://www.audiotool.com/album/0cszualx/",
-    "https://www.audiotool.com/album/dgj432vk7/",
-    "https://www.audiotool.com/album/fphr89zyy/",
-    "https://www.audiotool.com/album/drwan2/",
-    "https://www.audiotool.com/album/fg0yzv/",
-    "https://www.audiotool.com/album/zgmrmokl0/",
-    "https://www.audiotool.com/album/dyz0s22ey/",
-    "https://www.audiotool.com/album/0hqoaaw/",
-    "https://www.audiotool.com/album/s1xjwzjco/",
-    "https://www.audiotool.com/album/dejfpldko/",
-    "https://www.audiotool.com/album/q9ovx/",
-    "https://www.audiotool.com/album/s1nfsuq/",
-    "https://www.audiotool.com/album/8n41y2t4x/",
-    "https://www.audiotool.com/album/ku50sqn/",
-    "https://www.audiotool.com/album/vranm/",
-    "https://www.audiotool.com/album/akswas7/",
-    "https://www.audiotool.com/album/fpkdql8j/",
-    "https://www.audiotool.com/album/pif3btmq/",
-    "https://www.audiotool.com/album/ixieojt/",
-    "https://www.audiotool.com/album/jvjotpw/",
-    "https://www.audiotool.com/album/fckpbo/",
-    "https://www.audiotool.com/album/pri3aji/",
-    "https://www.audiotool.com/album/o15is/",
-    "https://www.audiotool.com/album/huq36h9yv/",
-    "https://www.audiotool.com/album/4oqrg5xs/",
-    "https://www.audiotool.com/album/d6qdh/",
-    "https://www.audiotool.com/album/qd3ss/",
-    "https://www.audiotool.com/album/jbkwf/",
-    "https://www.audiotool.com/album/mlzmzzie/",
-    "https://www.audiotool.com/album/0oryk5/",
-    "https://www.audiotool.com/album/gerzj5pa7/",
-    "https://www.audiotool.com/album/ofu4hck4/",
-    "https://www.audiotool.com/album/tzgcys/",
-    "https://www.audiotool.com/album/8v9sxpke3/",
-    "https://www.audiotool.com/album/blgcbm/",
-    "https://www.audiotool.com/album/fegwtffwt/",
-    "https://www.audiotool.com/album/qbtlhcu/",
-    "https://www.audiotool.com/album/hbufuuw/",
-    "https://www.audiotool.com/album/advfxu/",
-    "https://www.audiotool.com/album/e0aqv0xnj/",
-    "https://www.audiotool.com/album/0ybmt/",
-    "https://www.audiotool.com/album/cvguob117/",
-    "https://www.audiotool.com/album/ttfrr/",
-    "https://www.audiotool.com/album/xmilsqmwn/",
-    "https://www.audiotool.com/album/2sem3/",
-    "https://www.audiotool.com/album/jkuptea/",
-    "https://www.audiotool.com/album/ftnagxh/",
-    "https://www.audiotool.com/album/rjggctqy/",
-    "https://www.audiotool.com/album/7a9ktldnm/",
-    "https://www.audiotool.com/album/3i0j5p/",
-    "https://www.audiotool.com/album/mhksqxj/",
-    "https://www.audiotool.com/album/rs5yxjze/",
-    "https://www.audiotool.com/album/w3ob3/",
-    "https://www.audiotool.com/album/naaejqfyw/",
-    "https://www.audiotool.com/album/develp/",
-    "https://www.audiotool.com/album/tzvj27b/",
-    "https://www.audiotool.com/album/gw9cqiqr/",
-    "https://www.audiotool.com/album/xdi9i0mh/",
-    "https://www.audiotool.com/album/pohsz7b/",
-    "https://www.audiotool.com/album/qbemlckoi/",
-    "https://www.audiotool.com/album/ubguzjkis/",
-    "https://www.audiotool.com/album/knl9rxjg/",
-    "https://www.audiotool.com/album/bwjaynh12/",
-    "https://www.audiotool.com/album/mzcfu/",
-    "https://www.audiotool.com/album/pjpiou3l/",
-    "https://www.audiotool.com/album/zxigd/",
-    "https://www.audiotool.com/album/dvf4umd/",
-    "https://www.audiotool.com/album/mogada75n/",
-    "https://www.audiotool.com/album/p1yi4n/",
-    "https://www.audiotool.com/album/x8x8lg2tm/",
-    "https://www.audiotool.com/album/pevrv/",
-    "https://www.audiotool.com/album/uq8tpudy/",
-    "https://www.audiotool.com/album/mjv7t0sad/",
-    "https://www.audiotool.com/album/9pfmz/",
-    "https://www.audiotool.com/album/rwhjt2i/",
-    "https://www.audiotool.com/album/jtpwkjj/",
-    "https://www.audiotool.com/album/wmnofedc/",
-    "https://www.audiotool.com/album/n2xjvl/",
-    "https://www.audiotool.com/album/0i3ls/",
-    "https://www.audiotool.com/album/fynka1b/",
-    "https://www.audiotool.com/album/xe89crjwc/",
-    "https://www.audiotool.com/album/dq1pabol/",
-    "https://www.audiotool.com/album/qafr9/",
-    "https://www.audiotool.com/album/djnov/",
-    "https://www.audiotool.com/album/f1ailrc/",
+    ("https://www.audiotool.com/album/9pnx5/", "https://www.audiotool.com/user/ewan_mcculloch/"),
+    ("https://www.audiotool.com/album/hnwkpu4/", "https://www.audiotool.com/user/xavrockbeats/"),
+    ("https://www.audiotool.com/album/dgaoqtpm/", "https://www.audiotool.com/user/intracktion/"),
+    ("https://www.audiotool.com/album/0poc9tl/", "https://www.audiotool.com/user/flying-baby-seal/"),
+    ("https://www.audiotool.com/album/6pvw3rdg/", "https://www.audiotool.com/user/zonemusic/"),
+    ("https://www.audiotool.com/album/i0qv2qyt6/", "https://www.audiotool.com/user/trancefreak12/"),
+    ("https://www.audiotool.com/album/zwhdmh/", "https://www.audiotool.com/user/tocaknox/"),
+    ("https://www.audiotool.com/album/agr87qx/", "https://www.audiotool.com/user/mbelow/"),
+    ("https://www.audiotool.com/album/pts4e/", "https://www.audiotool.com/user/beat123/"),
+    ("https://www.audiotool.com/album/2gvqxec/", "https://www.audiotool.com/user/vltra2/"),
+    ("https://www.audiotool.com/album/dfkvkal/", "https://www.audiotool.com/user/shakey63/"),
+    ("https://www.audiotool.com/album/trxsjo/", "https://www.audiotool.com/user/opaqity/"),
+    ("https://www.audiotool.com/album/rpzwcp4v/", "https://www.audiotool.com/user/whitegrizzly/"),
+    ("https://www.audiotool.com/album/xlv4lr4/", "https://www.audiotool.com/user/psychosismusic/"),
+    ("https://www.audiotool.com/album/amg2pbi/", "https://www.audiotool.com/user/1n50mn1ac/"),
+    ("https://www.audiotool.com/album/og6fd6z/", "https://www.audiotool.com/user/jordynth/"),
+    ("https://www.audiotool.com/album/kcvqjvpm2/", "https://www.audiotool.com/user/nionsomnia/"),
+    ("https://www.audiotool.com/album/8ktqn1orf/", "https://www.audiotool.com/user/farcio/"),
+    ("https://www.audiotool.com/album/t5tdmds/", "https://www.audiotool.com/user/jambam/"),
+    ("https://www.audiotool.com/album/76ipsvb/", "https://www.audiotool.com/user/musicmanpw/"),
+    ("https://www.audiotool.com/album/0cszualx/", "https://www.audiotool.com/user/bocar_b-st/"),
+    ("https://www.audiotool.com/album/dgj432vk7/", "https://www.audiotool.com/user/grotzo/"),
+    ("https://www.audiotool.com/album/fphr89zyy/", "https://www.audiotool.com/user/oedipax/"),
+    ("https://www.audiotool.com/album/drwan2/", "https://www.audiotool.com/user/oedipax/"),
+    ("https://www.audiotool.com/album/fg0yzv/", "https://www.audiotool.com/user/fontana/"),
+    ("https://www.audiotool.com/album/zgmrmokl0/", "https://www.audiotool.com/user/bluedude/"),
+    ("https://www.audiotool.com/album/dyz0s22ey/", "https://www.audiotool.com/user/khoi98/"),
+    ("https://www.audiotool.com/album/0hqoaaw/", ""),
+    ("https://www.audiotool.com/album/s1xjwzjco/", ""),
+    ("https://www.audiotool.com/album/dejfpldko/", ""),
+    ("https://www.audiotool.com/album/q9ovx/", ""),
+    ("https://www.audiotool.com/album/s1nfsuq/", ""),
+    ("https://www.audiotool.com/album/8n41y2t4x/", ""),
+    ("https://www.audiotool.com/album/ku50sqn/", ""),
+    ("https://www.audiotool.com/album/vranm/", ""),
+    ("https://www.audiotool.com/album/akswas7/", ""),
+    ("https://www.audiotool.com/album/fpkdql8j/", ""),
+    ("https://www.audiotool.com/album/pif3btmq/", ""),
+    ("https://www.audiotool.com/album/ixieojt/", ""),
+    ("https://www.audiotool.com/album/jvjotpw/", ""),
+    ("https://www.audiotool.com/album/fckpbo/", ""),
+    ("https://www.audiotool.com/album/pri3aji/", ""),
+    ("https://www.audiotool.com/album/o15is/", ""),
+    ("https://www.audiotool.com/album/huq36h9yv/", ""),
+    ("https://www.audiotool.com/album/4oqrg5xs/", ""),
+    ("https://www.audiotool.com/album/d6qdh/", ""),
+    ("https://www.audiotool.com/album/qd3ss/", ""),
+    ("https://www.audiotool.com/album/jbkwf/", ""),
+    ("https://www.audiotool.com/album/mlzmzzie/", ""),
+    ("https://www.audiotool.com/album/0oryk5/", ""),
+    ("https://www.audiotool.com/album/gerzj5pa7/", ""),
+    ("https://www.audiotool.com/album/ofu4hck4/", ""),
+    ("https://www.audiotool.com/album/tzgcys/", ""),
+    ("https://www.audiotool.com/album/8v9sxpke3/", ""),
+    ("https://www.audiotool.com/album/blgcbm/", ""),
+    ("https://www.audiotool.com/album/fegwtffwt/", ""),
+    ("https://www.audiotool.com/album/qbtlhcu/", ""),
+    ("https://www.audiotool.com/album/hbufuuw/", ""),
+    ("https://www.audiotool.com/album/advfxu/", ""),
+    ("https://www.audiotool.com/album/e0aqv0xnj/", ""),
+    ("https://www.audiotool.com/album/0ybmt/", ""),
+    ("https://www.audiotool.com/album/cvguob117/", ""),
+    ("https://www.audiotool.com/album/ttfrr/", ""),
+    ("https://www.audiotool.com/album/xmilsqmwn/", ""),
+    ("https://www.audiotool.com/album/2sem3/", ""),
+    ("https://www.audiotool.com/album/jkuptea/", ""),
+    ("https://www.audiotool.com/album/ftnagxh/", ""),
+    ("https://www.audiotool.com/album/rjggctqy/", ""),
+    ("https://www.audiotool.com/album/7a9ktldnm/", ""),
+    ("https://www.audiotool.com/album/3i0j5p/", ""),
+    ("https://www.audiotool.com/album/mhksqxj/", ""),
+    ("https://www.audiotool.com/album/rs5yxjze/", ""),
+    ("https://www.audiotool.com/album/w3ob3/", ""),
+    ("https://www.audiotool.com/album/naaejqfyw/", ""),
+    ("https://www.audiotool.com/album/develp/", ""),
+    ("https://www.audiotool.com/album/tzvj27b/", ""),
+    ("https://www.audiotool.com/album/gw9cqiqr/", ""),
+    ("https://www.audiotool.com/album/xdi9i0mh/", ""),
+    ("https://www.audiotool.com/album/pohsz7b/", ""),
+    ("https://www.audiotool.com/album/qbemlckoi/", ""),
+    ("https://www.audiotool.com/album/ubguzjkis/", ""),
+    ("https://www.audiotool.com/album/knl9rxjg/", ""),
+    ("https://www.audiotool.com/album/bwjaynh12/", ""),
+    ("https://www.audiotool.com/album/mzcfu/", ""),
+    ("https://www.audiotool.com/album/pjpiou3l/", ""),
+    ("https://www.audiotool.com/album/zxigd/", ""),
+    ("https://www.audiotool.com/album/dvf4umd/", ""),
+    ("https://www.audiotool.com/album/mogada75n/", ""),
+    ("https://www.audiotool.com/album/p1yi4n/", ""),
+    ("https://www.audiotool.com/album/x8x8lg2tm/", ""),
+    ("https://www.audiotool.com/album/pevrv/", ""),
+    ("https://www.audiotool.com/album/uq8tpudy/", ""),
+    ("https://www.audiotool.com/album/mjv7t0sad/", ""),
+    ("https://www.audiotool.com/album/9pfmz/", ""),
+    ("https://www.audiotool.com/album/rwhjt2i/", ""),
+    ("https://www.audiotool.com/album/jtpwkjj/", ""),
+    ("https://www.audiotool.com/album/wmnofedc/", ""),
+    ("https://www.audiotool.com/album/n2xjvl/", ""),
+    ("https://www.audiotool.com/album/0i3ls/", ""),
+    ("https://www.audiotool.com/album/fynka1b/", ""),
+    ("https://www.audiotool.com/album/xe89crjwc/", ""),
+    ("https://www.audiotool.com/album/dq1pabol/", ""),
+    ("https://www.audiotool.com/album/qafr9/", ""),
+    ("https://www.audiotool.com/album/djnov/", ""),
+    ("https://www.audiotool.com/album/f1ailrc/", ""),
 ]
 
 HEADERS = {
@@ -141,7 +141,7 @@ def fetch_user_avatar(user_key):
         pass
     return ''
 
-def fetch_album(album_url):
+def fetch_album(album_url, user_url=''):
     html = fetch_url(album_url)
 
     # Extract artist name from title "Edition Audiotool: {Artist}"
@@ -202,14 +202,16 @@ def fetch_album(album_url):
     if not tracks:
         raise Exception('No tracks parsed from HTML')
 
-    # Get at_profile from last track's user - last user key in HTML
-    at_profile = ''
+    # Get at_profile and cover from user_url if provided, else from last track
+    at_profile = user_url if user_url else ''
     cover_url = ''
-    if track_user_keys:
-        # Last user key belongs to last track's artist
+    if at_profile:
+        m = re.search(r'/user/([^/]+)/?$', at_profile)
+        if m:
+            cover_url = fetch_user_avatar(m.group(1))
+    elif track_user_keys:
         last_user_key = track_user_keys[-1]
         at_profile = f'https://www.audiotool.com/user/{last_user_key}/'
-        # Fetch avatar for cover
         cover_url = fetch_user_avatar(last_user_key)
 
     return artist_name, at_profile, date, photo, cover_url, tracks
@@ -227,13 +229,14 @@ existing_by_url = {a.get('album_url', '').rstrip('/'): a for a in existing}
 results = []
 failed = []
 
-for i, album_url in enumerate(albums):
+for i, entry in enumerate(albums):
+    album_url, user_url = entry if isinstance(entry, tuple) else (entry, '')
     clean_url = album_url.rstrip('/')
     existing_entry = existing_by_url.get(clean_url)
 
     print(f'[{i+1}/{len(albums)}] {album_url}', end=' ', flush=True)
     try:
-        artist_name, at_profile, date, photo, cover_url, tracks = fetch_album(album_url)
+        artist_name, at_profile, date, photo, cover_url, tracks = fetch_album(album_url, user_url)
 
         if existing_entry:
             existing_entry['artist'] = artist_name
